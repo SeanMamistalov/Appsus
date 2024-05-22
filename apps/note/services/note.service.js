62888412febaa1e4e784e5481f7eb42c48c52d96
DanielY <145760623+DANIELYACOVI@users.noreply.github.com>
2024-05-22 11:35:40 +0300

    Create NoteService

--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,41 +1,86 @@
-const notes = [
-    {
-        id: 'n101',
-        createdAt: 1112222,
-        type: 'NoteTxt',
-        isPinned: true,
-        style: {
-            backgroundColor: '#00d'
-        },
-        info: {
-            txt: 'Fullstack Me Baby!'
-        }
-    },
-    {
-        id: 'n102',
-        type: 'NoteImg',
-        isPinned: false,
-        info: {
-            url: 'http://some-img/me',
-            title: 'Bobi and Me'
-        },
-        style: {
-            backgroundColor: '#00d'
-        }
-    },
-    {
-        id: 'n103',
-        type: 'NoteTodos',
-        isPinned: false,
-        info: {
-            title: 'Get my stuff together',
-            todos: [
-                {
-                    txt: 'Driving license'
-                    , doneAt: null
-                },
-                { txt: 'Coding power', doneAt: 187111111 }
-            ]
-        }
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const NOTE_KEY = 'noteDB'
+
+_createNotes()
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+    getEmptyNote,
+    getFilterFromSearchParams,
+}
+
+function query(filterBy = {}) {
+    return storageService.query(NOTE_KEY)
+        .then(notes => {
+            if (filterBy.title) {
+                const regExp = new RegExp(filterBy.title, 'i')
+                notes = notes.filter(note => regExp.test(note.title))
+            }
+            if (filterBy.type) {
+                notes = notes.filter(note => note.type === filterBy.type)
+            }
+            return notes
+        })
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId)
+        .then(note => note)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
     }
-]
+}
+
+function getEmptyNote(type = '', title = '') {
+    return { type, title }
+}
+
+function getFilterFromSearchParams(searchParams) {
+    return {
+        type: searchParams.get('type') || '',
+        title: searchParams.get('title') || '',
+    }
+}
+
+function _createNotes() {
+    let notes = utilService.loadFromStorage(NOTE_KEY)
+    if (!notes || !notes.length) {
+        notes = []
+        for (let i = 0; i < 5; i++) {
+            const colors = [
+                '#fff', '#ffb4b4', '#b4ffe0', '#b4b7ff', '#f9b4ff', 
+                '#c0e794', '#91c6f0'
+                
+            ]
+            const note = {
+                id: utilService.makeId(5),
+                createdAt: utilService.randomPastTime().toLocaleString(),
+                type: 'NoteTxt',
+                isPinned: true,
+                style: {
+                    backgroundColor: colors[utilService.getRandomIntInclusive(0, colors.length - 1)]
+                },
+                info: {
+                    title: utilService.makeLorem(2),
+                    txt: utilService.makeLorem(utilService.getRandomIntInclusive(2, 10))
+                }
+            }
+            notes.push(note)
+        }
+        utilService.saveToStorage(NOTE_KEY, notes)
+    }
+}
