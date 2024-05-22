5a5e4c972d1799e5150d439085391a832470fe09
Sean <mamistalovsean@gmail.com>
2024-05-22 21:37:21 +0300

    Update mail list

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,15 +1,20 @@
-import { MailPreview } from ".cmps/MailPreview.jsx"
+import { MailPreview } from '../cmps/MailPreview.jsx'
 
-export function MailList({ mails }) {
+export function MailList({ mails, onRemove }) {
+    if (!mails || mails.length === 0) {
+        return <div>No mails to display</div>;
+    }
+
     return (
         <section className="mail-list">
             <ul>
                 {mails.map(mail => (
                     <li key={mail.id}>
                         <MailPreview mail={mail} />
+                        <button onClick={() => onRemove(mail.id)}>X</button>
                     </li>
                 ))}
             </ul>
         </section>
-    )
+    );
 }
