c69c273e156b41a623826872959282b57d111a1a
Sean <mamistalovsean@gmail.com>
2024-05-22 21:37:05 +0300

    Update mail sidebar

---
+++ apps/mail/views/MailSidebar.jsx
@@ -0,0 +1,27 @@
+export function MailSidebar() {
+    return (
+        <div>
+            <header>
+                <span className="material-icons">menu</span>
+                <img className="gmail-logo" src="assets/img/gmail_logo.png" alt="gmail-logo" />
+            </header>
+            <div className="sidebar-gmail">
+                <div className="sidebar-item inbox">
+                    <span className="material-icons">inbox</span> Inbox
+                </div>
+                <div className="sidebar-item starred">
+                    <span className="material-icons">star</span> Starred
+                </div>
+                <div className="sidebar-item sent">
+                    <span className="material-icons">send</span> Sent
+                </div>
+                <div className="sidebar-item trash">
+                    <span className="material-icons">delete</span> Trash
+                </div>
+                <div className="sidebar-item drafts">
+                    <span className="material-icons">drafts</span> Drafts
+                </div>
+            </div>
+        </div>
+    )
+}
