3bf58d11d71adc2138d71e546ea5f5a957825ca4
Sean <mamistalovsean@gmail.com>
2024-05-22 21:37:15 +0300

    Update mail index

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,47 +1,46 @@
-// const { useEffect, useState } = React
-// import { MailPreview } from '../apps/mail/cmps/MailPreview.jsx'
-// import { emailService } from '../apps/mail/services/mail.service.js'
+const { useState, useEffect } = React
+const { useSearchParams } = ReactRouterDOM
+import { emailService } from '../services/mail.service.js'
+import { showSuccessMsg, showErrorMsg } from '../../../services/event-bus.service.js'
+import { MailList } from '../cmps/MailList.jsx'
+
+function getFilterFromSearchParams(searchParams) {
+    const filter = {};
+    for (const [key, value] of searchParams.entries()) {
+        filter[key] = value;
+    }
+    return filter;
+}
 
 export function MailIndex() {
-    // const [mails, setMails] = useState([])
+    const [mails, setMails] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [filterBy, setFilterBy] = useState(getFilterFromSearchParams(searchParams));
+    useEffect(() => {
+        setSearchParams(filterBy)
+        emailService.query(filterBy)
+            .then(mails => setMails(mails))
+            .catch(err => showErrorMsg('Failed to fetch notes'))
+    }, [filterBy, setSearchParams])
 
-    // useEffect(() => {
-    //     emailService.createMails()
+    function onSetFilterBy(newFilter) {
+        setFilterBy(newFilter)
+    }
 
-    //     emailService.query({ status: 'inbox' })
-    //         .then(fetchedMails => {
-    //             setMails(fetchedMails)
-    //         })
-    // }, [])
+    function removeMail(mailId) {
+        emailService.remove(mailId)
+            .then(() => {
+                setNotes(prevMails => prevMails.filter(mail => mail.id !== mailId))
+                showSuccessMsg(`mail ${mailId} removed successfully!`)
+            })
+            .catch(err => {
+                showErrorMsg('Failed to remove mail')
+            })
+    }
 
     return (
-        <div>
-            <header>
-                <span className="material-icons">menu</span>
-                <img className="gmail-logo" src="assets/img/gmail_logo.png" alt="gmail-logo" />
-            </header>
-            <div className="sidebar-gmail">
-                <div className="sidebar-item inbox">
-                    <span className="material-icons">inbox</span> Inbox
-                </div>
-                <div className="sidebar-item starred">
-                    <span className="material-icons">star</span> Starred
-                </div>
-                <div className="sidebar-item sent">
-                    <span className="material-icons">send</span> Sent
-                </div>
-                <div className="sidebar-item trash">
-                    <span className="material-icons">delete</span> Trash
-                </div>
-                <div className="sidebar-item drafts">
-                    <span className="material-icons">drafts</span> Drafts
-                </div>
-            </div>
-            {/* <div className="mail-list">
-                {mails.map(mail => (
-                    <MailPreview key={mail.id} mail={mail} />
-                ))}
-            </div> */}
-        </div>
+        <section>
+            <MailList mails={mails} onRemove={removeMail} />
+        </section>
     )
-}
+ }
