57ff92472fec67a400eecee49caf6c08fefa3202
Sean <mamistalovsean@gmail.com>
2024-05-25 21:02:59 +0300

    Update mailservice & index

--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -1,48 +1,72 @@
-const { useState, useEffect } = React
-const { useSearchParams } = ReactRouterDOM
-import { emailService } from '../services/mail.service.js'
-import { showSuccessMsg, showErrorMsg } from '../../../services/event-bus.service.js'
-import { MailList } from '../cmps/MailList.jsx'
-import { MailSidebar } from './MailSidebar.jsx'
+const { useState, useEffect } = React;
+const { useSearchParams } = ReactRouterDOM;
+import { emailService } from "../services/mail.service.js";
+import {
+  showSuccessMsg,
+  showErrorMsg,
+} from "../../../services/event-bus.service.js";
+import { MailList } from "../cmps/MailList.jsx";
+import { MailSidebar } from "./MailSidebar.jsx";
 
 function getFilterFromSearchParams(searchParams) {
-    const filter = {};
-    for (const [key, value] of searchParams.entries()) {
-        filter[key] = value;
-    }
-    return filter;
+  const filter = {};
+  for (const [key, value] of searchParams.entries()) {
+    filter[key] = value;
+  }
+  return filter;
 }
 
 export function MailIndex() {
-    const [mails, setMails] = useState([])
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [filterBy, setFilterBy] = useState(getFilterFromSearchParams(searchParams));
-    useEffect(() => {
-        setSearchParams(filterBy)
-        emailService.query(filterBy)
-            .then(mails => setMails(mails))
-            .catch(err => showErrorMsg('Failed to fetch notes'))
-    }, [filterBy, setSearchParams])
+  const [mails, setMails] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [filterBy, setFilterBy] = useState(
+    getFilterFromSearchParams(searchParams)
+  );
 
-    function onSetFilterBy(newFilter) {
-        setFilterBy(newFilter)
-    }
+  useEffect(() => {
+    setSearchParams(filterBy);
+    emailService
+      .query(filterBy)
+      .then((mails) => setMails(mails))
+      .catch((err) => showErrorMsg("Failed to fetch notes"));
+  }, [filterBy, setSearchParams]);
 
-    function removeMail(mailId) {
-        emailService.remove(mailId)
-            .then(() => {
-                setNotes(prevMails => prevMails.filter(mail => mail.id !== mailId))
-                showSuccessMsg(`mail ${mailId} removed successfully!`)
-            })
-            .catch(err => {
-                showErrorMsg('Failed to remove mail')
-            })
-    }
+  function onSetFilterBy(newFilter) {
+    setFilterBy(newFilter);
+  }
 
-    return (
-        <section>
-            <MailSidebar/>
-            <MailList mails={mails} onRemove={removeMail} />
-        </section>
-    )
- }
+  function removeMail(mailId) {
+    emailService
+      .remove(mailId)
+      .then(() => {
+        setMails((prevMails) => prevMails.filter((mail) => mail.id !== mailId));
+        showSuccessMsg(`mail ${mailId} removed successfully!`);
+      })
+      .catch((err) => {
+        showErrorMsg("Failed to remove mail");
+      });
+  }
+
+  function markAsRead(mailId) {
+    emailService
+      .markAsRead(mailId)
+      .then(() => {
+        setMails((prevMails) => {
+          return prevMails.map((mail) => {
+            if (mail.id === mailId) {
+              return { ...mail, isRead: true };
+            }
+            return mail;
+          });
+        });
+      })
+      .catch((err) => showErrorMsg("Failed to mark mail as read"));
+  }
+
+  return (
+    <section className="mail-index">
+      <MailSidebar />
+      <MailList mails={mails} onRemove={removeMail} onMarkAsRead={markAsRead} />
+    </section>
+  );
+}
