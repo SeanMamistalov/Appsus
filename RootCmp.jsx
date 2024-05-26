6334162823bbff8d42d596f096412bfd19f31e96
DanielY <145760623+DANIELYACOVI@users.noreply.github.com>
2024-05-26 08:19:00 +0300

    Navigate fix

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -4,20 +4,14 @@ const Router = ReactRouterDOM.HashRouter
 import { AppHeader } from "./cmps/AppHeader.jsx"
 import { About } from "./views/About.jsx"
 import { Home } from "./views/Home.jsx"
-
+import { MailIndex } from "./apps/mail/views/MailIndex.jsx"
+import { EmailDetails } from "./apps/mail/views/MailDetails.jsx";
 import { NoteIndex } from "./apps/note/views/NoteIndex.jsx"
 import { Trash } from "./apps/note/views/Trash.jsx"
 import { Archives } from "./apps/note/views/Archives.jsx"
 import { NoteList } from "./apps/note/views/NoteList.jsx"
 import { Reminders } from "./apps/note/views/Reminders.jsx"
 import { EditLabels } from "./apps/note/views/Editlabels.jsx"
-
-import { MailIndex } from "./apps/mail/views/MailIndex.jsx"
-import { EmailDetails } from "./apps/mail/views/MailDetails.jsx";
-import { MailStarred } from "./apps/mail/views/Starred.jsx"
-import { MailSent } from "./apps/mail/views/Sent.jsx"
-import { MailTrash } from "./apps/mail/views/Trash.jsx"
-import { MailDrafts } from "./apps/mail/views/Drafts.jsx"
 
 export function App() {
     return <Router>
@@ -26,15 +20,8 @@ export function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                
                 <Route path="/mail" element={<MailIndex />} />
                 <Route path="/mail/:mailId" element={<EmailDetails />} />
-                <Route path="/mail/inbox" element={<MailIndex />}/>
-                <Route path="/mail/starred" element={<MailStarred />}/>
-                <Route path="/mail/sent" element={<MailSent />}/>
-                <Route path="/mail/trash" element={<MailTrash />}/>
-                <Route path="/mail/drafts" element={<MailDrafts />}/>
-
                 <Route path="/note" element={<NoteIndex />} >
                 <Route index element={<Navigate to="/note/noteList" />} />
                     <Route path="/note/noteList" element={<NoteList />}/>
