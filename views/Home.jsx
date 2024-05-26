1e056e82bef5299b0e09bb6e7177fc274a348b7f
Sean <mamistalovsean@gmail.com>
2024-05-26 23:38:09 +0300

    Update About & Home pages

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,15 @@
+const { Link } = ReactRouterDOM
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
+  return (
+    <section className="home-view">
+      <div className="hero">
+        <h1>Welcome to Appsus!</h1>
+        <p>We are here to provide you with top-notch development services.</p>
+        <Link to="/mail"><button className="btn">Mail</button></Link>
+        <Link to="/note"><button className="btn">Notes</button></Link>
+        <Link to="/book"><button className="btn">Books</button></Link>
+      </div>
     </section>
+  );
 }
