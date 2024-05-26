3005eca31d9b9c803e684290f998aedabc42a74a
Sean <mamistalovsean@gmail.com>
2024-05-27 00:18:11 +0300

    Edit home page

--- views/Home.jsx
+++ views/Home.jsx
@@ -6,7 +6,7 @@ export function Home() {
       <div className="hero">
       <img src="assets/img/horse-logo.jpg" alt="Horse Logo" className="logo" /> 
         <h1>Welcome to Appsus!</h1>
-        <p>We are here to provide you with top-notch development services.</p>
+        <p>We are here to provide you with top-notch applications.</p>
         <Link to="/mail"><button className="btn">Mail</button></Link>
         <Link to="/note"><button className="btn">Notes</button></Link>
         <Link to="/book"><button className="btn">Books</button></Link>
