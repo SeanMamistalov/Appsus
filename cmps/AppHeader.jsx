83002080daefd2920098247ed1604f2b831bb512
DanielY <145760623+DANIELYACOVI@users.noreply.github.com>
2024-06-02 22:44:26 +0300

    App header styling

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,18 +1,34 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+    const [showDropdown, setShowDropdown] = useState(false)
 
-    return <header className="app-header">
-        <Link to="/"
-            style={{ textDecoration: 'none'}}>
-            <h3>Appsus</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-            <NavLink to="/book">Books</NavLink>
-        </nav>
-    </header>
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown)
+    }
+
+    return (
+        <header className="app-header">
+            <Link to="/" style={{ textDecoration: 'none' }}>
+                <h3>Appsus</h3>
+            </Link>
+            <nav>
+                <div className="dropdown">
+                    <button onClick={toggleDropdown} className="dropdown-button">
+                        <img src="assets/img/menu.svg" alt="More options" className="dropdown-icon" />
+                    </button>
+                    {showDropdown && (
+                        <div className="dropdown-content">
+                            <Link to="/">Home</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/mail">Mail</Link>
+                            <Link to="/note">Notes</Link>
+                            <Link to="/book">Books</Link>
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
 }
